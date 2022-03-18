3ad0504647afe57dfd1e85de3e913f49d7d0c88f
Andra Antariksa <andra.antariksa@gmail.com>
2022-03-18 21:38:19 +0700

    Change the theme colors

--- app/src/main/java/id/shaderboi/cata/ui/theme/Theme.kt
+++ app/src/main/java/id/shaderboi/cata/ui/theme/Theme.kt
@@ -1,20 +1,15 @@
 package id.shaderboi.cata.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalContext
-import id.shaderboi.cata.util.ThemePref
-import id.shaderboi.cata.util.dataStore
-import kotlinx.coroutines.flow.collectLatest
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun CataAppTheme(theme: Theme, content: @Composable () -> Unit) {
     val colors = when (theme) {
-        Theme.Dark -> darkColors()
+        Theme.Dark -> darkColors(primary = Color(0xff99c3c1))
         Theme.Light -> lightColors()
     }
 
